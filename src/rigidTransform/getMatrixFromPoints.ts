9d19b2f27a59a2b40de3aac22b3931957a743267
Océane Patiny <oceane@patiny.com>
2023-05-25 13:52:27 +0200

    feat: implement getMatrixFromPoints

---
+++ src/rigidTransform/getMatrixFromPoints.ts
@@ -0,0 +1,34 @@
+import Matrix from 'ml-matrix';
+
+/**
+ * Coordinates of a point in an image with the top-left corner being the reference point.
+ */
+export interface Point {
+  /**
+   * Point row.
+   *
+   */
+  row: number;
+  /**
+   * Point column.
+   *
+   */
+  column: number;
+}
+
+/**
+ * Convert row/column points to a matrix.
+ * Last row is padded with zeros (ignore third dimension).
+ *
+ * @param points - Points to process.
+ * @returns The matrix.
+ */
+export function getMatrixFromPoints(points: Point[]): Matrix {
+  let matrix = new Matrix(points.length, 3);
+  for (let i = 1; i < points.length; i++) {
+    matrix.set(0, i, points[i].column);
+    matrix.set(1, i, points[i].row);
+  }
+
+  return matrix;
+}
