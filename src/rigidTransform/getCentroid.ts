c471a29911ba3d5e76f72d9cffdbf6720f39e8bc
Océane Patiny <oceane@patiny.com>
2023-05-25 13:52:09 +0200

    test: add getCentroid test

--- src/rigidTransform/getCentroid.ts
+++ src/rigidTransform/getCentroid.ts
@@ -1,18 +1,4 @@
-/**
- * Coordinates of a point in an image with the top-left corner being the reference point.
- */
-export interface Point {
-  /**
-   * Point row.
-   *
-   */
-  row: number;
-  /**
-   * Point column.
-   *
-   */
-  column: number;
-}
+import { Point } from './getMatrixFromPoints';
 
 /**
  * Compute the centroid of a set of points.
