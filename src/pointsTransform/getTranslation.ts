18688a62f6a9d6e6a99f12b863022e42e168c179
Océane Patiny <oceane@patiny.com>
2023-05-25 08:35:18 +0200

    feat: implement getTranslation

---
+++ src/pointsTransform/getTranslation.ts
@@ -0,0 +1,18 @@
+import { Point, getCentroid } from './getCentroid';
+// destination - source
+/**
+ * Compute the translation vector to add to source to get to destination.
+ *
+ * @param source - Source points.
+ * @param destination - Destination points.
+ * @returns The traslation vector from source to destination.
+ */
+export function getTranslation(source: Point[], destination: Point[]): Point {
+  const sourceCentroid = getCentroid(source);
+  const destinationCentroid = getCentroid(destination);
+
+  return {
+    row: destinationCentroid.row - sourceCentroid.row,
+    column: destinationCentroid.column - sourceCentroid.column,
+  };
+}
