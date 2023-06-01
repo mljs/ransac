fb3da82d8a813f7c55b2d93316e4fcb7f2d9fa79
Océane Patiny <oceane@patiny.com>
2023-06-01 08:34:55 +0200

    fix: bug with angle in degrees

---
+++ src/rigidTransform/createRigidTransformModel.ts
@@ -0,0 +1,29 @@
+import { ModelFunction } from '..';
+
+import { Point } from './getMatrixFromPoints';
+
+/**
+ * Generate a function that applies the given transformation parameters to a point.
+ *
+ * @param transform - Transformation to apply.
+ * @returns Transformed point.
+ */
+export function createRigidTransformModel(
+  transform: number[],
+): ModelFunction<Point> {
+  return (point: Point) => {
+    const angle = (transform[0] * Math.PI) / 180;
+    const xTranslation = transform[1];
+    const yTranslation = transform[2];
+    const column =
+      Math.cos(angle) * point.column -
+      Math.sin(angle) * point.row +
+      xTranslation;
+    const row =
+      Math.sin(angle) * point.column +
+      Math.cos(angle) * point.row +
+      yTranslation;
+
+    return { column, row };
+  };
+}
