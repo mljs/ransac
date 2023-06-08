840d880adf3afc2702350b26da6a417a94d8a00e
Océane Patiny <oceane@patiny.com>
2023-06-08 13:52:26 +0200

    fix: drawResults works

--- src/toMigrate/forImageJs/drawResult.ts
+++ src/toMigrate/forImageJs/drawResult.ts
@@ -1,4 +1,4 @@
-import { Point, Image, ImageColorModel } from 'image-js';
+import { Point, Image, ImageColorModel, Mask, writeSync } from 'image-js';
 
 /**
  * Draw source, destination and transformed points on an image.
@@ -18,38 +18,41 @@ export function drawResult(
   const center = (imageSide - 1) / 2;
   const image = new Image(imageSide, imageSide, {
     colorModel: ImageColorModel.RGB,
-  }).fill([255, 255, 255]);
+  });
 
   // draw coordinates axes
   image.drawLine(
     { row: center, column: 0 },
     { row: center, column: imageSide },
-    { out: image },
+    { out: image, strokeColor: [255, 255, 255] },
   );
   image.drawLine(
     { row: 0, column: center },
     { row: imageSide, column: center },
-    { out: image },
+    { out: image, strokeColor: [255, 255, 255] },
   );
 
   // draw points: source (green), destination (blue), result (red)
-  image.drawPoints(source, {
-    out: image,
-    color: [0, 255, 0],
-    origin: { row: center, column: center },
-  });
-
-  image.drawPoints(destination, {
-    out: image,
-    color: [0, 0, 255],
-    origin: { row: center, column: center },
-  });
-
-  image.drawPoints(result, {
-    out: image,
-    color: [255, 0, 0],
-    origin: { row: center, column: center },
-  });
+  drawBlendedPoints(source, [null, 255, null]);
+  writeSync(`${__dirname}/source.png`, image);
+  drawBlendedPoints(destination, [null, null, 255]);
+  writeSync(`${__dirname}/destination.png`, image);
+  drawBlendedPoints(result, [255, null, null]);
+  writeSync(`${__dirname}/result.png`, image);
 
   return image;
+
+  function drawBlendedPoints(points: Point[], color: (number | null)[]) {
+    const mask = new Mask(imageSide, imageSide);
+    const withPoints = mask.drawPoints(points, {
+      color: [1],
+      origin: { row: center, column: center },
+    });
+    writeSync(`${__dirname}/mask.png`, withPoints);
+    image.paintMask(withPoints, {
+      out: image,
+      color,
+      blend: false,
+    });
+  }
 }
