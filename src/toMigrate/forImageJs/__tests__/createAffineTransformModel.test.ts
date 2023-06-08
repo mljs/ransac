6a621a1ea5bc3c423be9cce7a6375bb18b0cc9ef
Océane Patiny <oceane@patiny.com>
2023-06-08 16:30:23 +0200

    refactor: migrate files to correct repositories

--- src/toMigrate/forImageJs/__tests__/createAffineTransformModel.test.ts
+++
@@ -1,7 +0,0 @@
-import { createAffineTransformModel } from '../createAffineTransformModel';
-
-test('wrong nb of parameters', () => {
-  expect(() => {
-    createAffineTransformModel([1, 1, 1, 1, 1]);
-  }).toThrow('Transform had wrong number of parameters');
-});
